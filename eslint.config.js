a7057984439e09d5ce93efaf476ca38ffb726d80
FloFlo-L <florian.lescribaa@gmail.com>
2026-01-28 12:34:49 +0100

    fix: update eslint ignores to include postcss config file

--- eslint.config.js
+++ eslint.config.js
@@ -73,7 +73,7 @@ export default [
         },
     },
     {
-        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js', 'vite.config.ts'],
+        ignores: ['vendor', 'node_modules', 'public', 'bootstrap/ssr', 'tailwind.config.js', 'vite.config.ts', 'postcss.config.cjs'],
     },
     prettier, // Turn off all rules that might conflict with Prettier
 ];
